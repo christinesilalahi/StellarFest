a9c836e984aadf7b32b09549720b571e435703fc
christinesilalahi <christinesilalahi12@gmail.com>
2024-12-18 23:08:57 +0700

    Update Validation

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -5,14 +5,9 @@ import model.User;
 public class UserController {
 
 	public static boolean Register(String email, String username, String password, String role) {
-		if(email.isEmpty() || username.isEmpty() || password.isEmpty() || role.isEmpty()) {
+		if(!checkRegisterInput(email, username, password, role)) {
 			return false;
 		}
-		
-		if(password.length() < 5) {
-			return false;
-		}
-		
 		return User.Register(email, username, password, role);
 	}
 
@@ -22,6 +17,30 @@ public class UserController {
 		}
 		
 		return User.Login(email, password);
+	}
+	
+	public static boolean checkRegisterInput(String email, String username, String password, String role) {
+		if(email.isEmpty() || username.isEmpty() || password.isEmpty() || role.isEmpty()) {
+			return false;
+		}
+		
+		if(!email.contains("@")) {
+			return false;
+		}
+		
+		if(password.length() < 5) {
+			return false;
+		}
+		
+		if(User.isEmailExists(email)) {
+			return false;
+		}
+		
+		if(User.isUsernameExists(username)) {
+			return false;
+		}
+		
+		return true;
 	}
 
 }
---
+++ src/view/EventOrganizerView.java
@@ -0,0 +1,9 @@
+package view;
+
+public class EventOrganizerView {
+
+	public EventOrganizerView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
